79e33c9962d6af197c8fde22d3dbdb0908460844
johnsavvidis <savvidisjohn1990@yahoo.gr>
2023-04-18 19:57:34 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,28 +1,28 @@
 const characterData = {
     hero: {
         name: "Wizard",
-        avatar: "wizard.png",
+        avatar: "./wizard.png",
         health: 60,
         diceCount: 3,
         currentDiceScore: []
     },
     orc: {
         name: "Orc",
-        avatar: "orc.png",
+        avatar: "./orc.png",
         health: 30,
         diceCount: 1,
         currentDiceScore: []
     },
     demon: {
         name: "Demon",
-        avatar: "demon.png",
+        avatar: "./demon.png",
         health: 25,
         diceCount: 2,
         currentDiceScore: []
     },
     goblin: {
         name: "Goblin",
-        avatar: "goblin.png",
+        avatar: "./goblin.png",
         health: 20,
         diceCount: 3,
         currentDiceScore: []
